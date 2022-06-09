2f86d9a478b65577ef88dd0bb559b3da8603443d
Marnin Audu <msmaudu@gmail.com>
2022-06-10 00:29:27 +0100

    Updated navbar code

--- tobi_site/src/components/navbar/navbar.js
+++ tobi_site/src/components/navbar/navbar.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import navbarcss from './navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>N</div>
+    <nav className={`${navbarcss.nav}`}>
+      <a href="/"><button className={`${navbarcss.navbtn}`}>Home</button></a>
+      <a href="/products"><button className={`${navbarcss.navbtn}`}>Products</button></a>
+    </nav>
   )
 }
 
