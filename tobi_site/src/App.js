805b443548354185cb009a1063c497a363a04cb8
Marnin Audu <msmaudu@gmail.com>
2022-06-09 08:10:27 +0100

    Added react-router-dom

--- tobi_site/src/App.js
+++ tobi_site/src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
+import Styles from'./App.module.css';
 import React from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
+const Navbar=lazy(()=>import('./components/navbar/navbar'));
+const Home=lazy(()=>import('./pages/home/home'));
 
 function App() {
   return (
-  <div>
-    <home></home>
-  </div>
+  <div className="App">
+    <Suspense fallback={<h1>Loadging...</h1>}>
+      <Navbar/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </Router>
+    </Suspense>
+    </div>
   );
 }
 
---
+++ tobi_site/src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LOGINCSS from './home.module.css'
+
+const Home = () => {
+  return (
+    <div className={LOGINCSS.IntroCard}>
+      <h1 className={LOGINCSS.head}><em>Earning 6 figures</em> from anywhere sounds like a dream or something you only see on TV. I'm here to tell you that it not only possible but you can do it to right here lets get started.</h1>
+
+      <article>HI, I'm ...<br/><br/><br/></article>
+    </div>
+  )
+}
+
+export default Home
